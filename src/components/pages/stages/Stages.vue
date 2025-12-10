90455b41b6423fbf425ae12d76b40d4e4850606c
Savanes13 <ilzz43373@gmail.com>
2025-12-11 00:05:41 +0300

    верстка страницы этапов

--- src/components/pages/stages/Stages.vue
+++ src/components/pages/stages/Stages.vue
@@ -82,7 +82,6 @@ const stabStages = {
     <PageHeader>
       Этапы
     </PageHeader>
-
     <div class="stages__items">
       <WrapperBlock>
         <div class="header-stage">
@@ -100,9 +99,83 @@ const stabStages = {
             </DefaultButton>
           </div>
         </div>
+        <div class="info-stage">
+          <div class="info-stage__item">
+            <div class="title-block">
+              <p>Дедлайны</p>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дата начала этапа</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.deadlines.startDate.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дедлайн возможности начать этап</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.deadlines.opportunityDate.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дедлайн отправки всех заданий</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.deadlines.deadlineAll.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Время на выполнение задания</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.deadlines.timeComplete }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="info-stage__item">
+            <div class="title-block">
+              <p>Оценка</p>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Проходной балл</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.evaluation.passingGrade }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Текущий диапазон оценки</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.evaluation.currentRange.min }}–{{ stabStages.stageFirst.evaluation.currentRange.max }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Оценки стуктуры и логики</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.evaluation.structureLogic.min }}–{{ stabStages.stageFirst.evaluation.structureLogic.max }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Оценки мотивации</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageFirst.evaluation.motivation.min }}–{{ stabStages.stageFirst.evaluation.motivation.max }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </WrapperBlock>
-  
-  
       <WrapperBlock>
         <div class="header-stage">
           <div class="header-stage__title">
@@ -119,14 +192,104 @@ const stabStages = {
             </DefaultButton>
           </div>
         </div>
+        <div class="info-stage">
+          <div class="info-stage__item">
+            <div class="title-block">
+              <p>Дедлайны</p>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дата начала этапа</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.deadlines.startDate.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дедлайн возможности начать этап</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.deadlines.opportunityDate.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Дедлайн отправки всех заданий</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.deadlines.deadlineAll.stringDate }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Время на выполнение задания</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.deadlines.timeComplete }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="info-stage__item">
+            <div class="title-block">
+              <p>Оценка</p>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Проходной балл</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.evaluation.passingGrade }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Текущий диапазон оценки</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.evaluation.currentRange.min }}–{{ stabStages.stageSecond.evaluation.currentRange.max }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Оценки стуктуры и логики</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.evaluation.structureLogic.min }}–{{ stabStages.stageSecond.evaluation.structureLogic.max }}</p>
+              </div>
+            </div>
+            <div class="item-block">
+              <div class="item-block__name">
+                <p>Оценки мотивации</p>
+              </div>
+              <div class="item-block__text">
+                <p>{{ stabStages.stageSecond.evaluation.motivation.min }}–{{ stabStages.stageSecond.evaluation.motivation.max }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </WrapperBlock>
-  
-  
       <WrapperBlock>
-        
+        <div class="header-stage">
+          <div class="header-stage__title">
+            <div class="counter">
+              <p>Этап 3</p>
+            </div>
+            <div class="name">
+              <p>{{ stabStages.stageThird.name }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="not-configurable">
+          <p>Этот этап не настраивается</p>
+        </div>
+        <div class="interview-button">
+          <DefaultButton class="default-button__color-green-transparent default-button__size--small">
+            К собеседованиям
+          </DefaultButton>
+        </div>
       </WrapperBlock>
     </div>
-
   </div>
 </template>
 
@@ -140,6 +303,7 @@ const stabStages = {
 .header-stage {
   display: flex;
   justify-content: space-between;
+  margin-bottom: 20px;
 }
 
 .header-stage__stage-button {
@@ -159,5 +323,58 @@ const stabStages = {
   font-size: 24px;
   line-height: 25px;
   color: #333333;
+}
+
+.info-stage {
+  display: flex;
+  gap: 20px;
+}
+
+.info-stage__item {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  border-radius: 16px;
+  border: 1px solid #D0D7E5;
+  padding: 20px;
+}
+
+.title-block {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 25px;
+  color: #333333;
+}
+
+.item-block {
+  display: flex;
+  justify-content: space-between;
+}
+
+.item-block__text {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #333333;
+}
+
+.item-block__name {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 25px;
+  color: #747F9C;
+}
+
+.not-configurable {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 25px;
+  color: #333333;
+  margin-bottom: 20px;
+}
+
+.interview-button {
+  width: 210px;
 }
 </style>
